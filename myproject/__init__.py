e68ebad4678d44e9be209d26d215363bd990ab64
josekang <josekangethe2@gmail.com>
2019-04-15 13:13:30 +0300

    commit message

---
+++ myproject/__init__.py
@@ -0,0 +1,17 @@
+import os
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config["SECRET_key"] = "!@#$%^&*()_+QWERTYUIOPASDFGHJKLZXCVBNM<>POIJUHYGTFRDERTYUIKJHBCFDXSDFGHJ"
+app.config["SQLALCHEMY_DATABASE_URI"] = "///sqlite" + os.path.join(basedir, "data.sqlite")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db=SQLAlchemy(app)
+
+Migrate(app, db)
