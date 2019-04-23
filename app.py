72671c57cda0df8960473b56e5a4e85517a52a7b
josekang <josekangethe2@gmail.com>
2019-04-23 16:45:43 +0300

    Added the contact form page using flask-wtforms module

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
-from flask import Flask, render_template
 
+from flask import Flask, render_template, url_for, redirect, flash, request
+from myproject import ContactForm
+from myproject import mail
+from flask_mail import Message
 from myproject import app
 
 #home route
-@app.route("/")
+@app.route("/", methods = ["GET", "POST"])
 def home():
+    form = ContactForm()
+    if request.method == "POST":
+        if form.validate() == False:
+            flash("All fields are required")
+            return render_template("home.html", form = form)
+
+        else:
+            msg = Message(form.subject.data, sender = "contact@example.com", recipients = ["josekangethe2@gmail.com"])
+            msg.body = """
+            From: %s &lt;%s&gt;
+            %s
+            """ % (form.name.data, form.email.data, form.message.data)
+            mail.send(msg)
+
+            return render_template("home.html", success = True)
+
+    elif request.method == "GET":
+        return render_template("home.html", form = form)
+
+
     return render_template("home.html")
 
 if __name__ == '__main__':
--- myproject/__init__.py
+++ myproject/__init__.py
@@ -1,8 +1,15 @@
 import os
 
-from flask import Flask
+from flask import Flask,request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_mail import Mail, Message
+from flask_wtf import FlaskForm
+from wtforms import TextField, TextAreaField, SubmitField
+from wtforms.validators import DataRequired, Email
+from wtforms.validators import ValidationError
+
+mail = Mail()
 
 app = Flask(__name__)
 
@@ -11,7 +18,21 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SECRET_KEY"] = "!@#$%^&*()_+QWERTYUIOPASDFGHJKLZXCVBNM<>POIJUHYGTFRDERTYUIKJHBCFDXSDFGHJ"
 app.config["SQLALCHEMY_DATABASE_URI"] = "///sqlite" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["MAIL_SERVER"] = "smtp.gmail.com"
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_SSL"] = True
+app.config["MAIL_USERNAME"] = "josekangethe2@gmail.com"
+app.config["MAIL_PASSWORD"] = "#39JoseKeliza"
 
 db=SQLAlchemy(app)
 
 Migrate(app, db)
+
+mail.__init__(app)
+
+class ContactForm(FlaskForm):
+    name = TextField("Name", validators = [DataRequired("The name field cannot be empty")])
+    email = TextField("Email", validators = [DataRequired(), Email("please enter a valid email address")])
+    subject = TextField("Subject", validators = [DataRequired("The subject field cannot be empty")])
+    message = TextAreaField("Message", validators = [DataRequired("The message field cannot be empty")])
+    submit = SubmitField("SEND")
