9697278c312109aedb5a37eb159df9b643c0ac62
josekang <josekangethe2@gmail.com>
2019-04-23 11:22:37 +0300

    Corrected the __init__.py file

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.config["SECRET_key"] = "!@#$%^&*()_+QWERTYUIOPASDFGHJKLZXCVBNM<>POIJUHYGTFRDERTYUIKJHBCFDXSDFGHJ"
+app.config["SECRET_KEY"] = "!@#$%^&*()_+QWERTYUIOPASDFGHJKLZXCVBNM<>POIJUHYGTFRDERTYUIKJHBCFDXSDFGHJ"
 app.config["SQLALCHEMY_DATABASE_URI"] = "///sqlite" + os.path.join(basedir, "data.sqlite")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
