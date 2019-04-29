966aa3940927d793aa741b9879b4b1bd0492d906
josekang <josekangethe2@gmail.com>
2019-04-29 15:07:06 +0300

    Added the footer section linking the web app to my social sites

--- myproject/__init__.py
+++ myproject/__init__.py
@@ -22,6 +22,7 @@ app.config["MAIL_SERVER"] = "smtp.gmail.com"
 app.config["MAIL_PORT"] = 465
 app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_USERNAME"] = "josekangethe2@gmail.com"
+app.config["MAIL_PASSWORD"] = "#39JoseKeliza"
 
 db=SQLAlchemy(app)
 
